0f2449b09521cd74f289f7a0498470b2611da611
Nikolaus Waxweiler <nikolaus.waxweiler@daltonmaag.com>
2021-05-24 18:28:18 +0100

    Initial commit

---
+++ gpos_compression_by_tetris/__main__.py
@@ -0,0 +1,96 @@
+import argparse
+import csv
+import sys
+from io import BytesIO
+from pathlib import Path
+from typing import Any, List, Optional, Tuple
+
+from fontTools.ttLib import TTFont
+
+from .compact_kern_feature_writer import compact
+
+# TODO: check performance with harfbuzz with broken up subtables
+
+
+def main(args: Optional[List[str]] = None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("fonts", type=Path, nargs="+", help="Path to TTFs.")
+    parsed_args = parser.parse_args()
+
+    rows = []
+    font_path: Path
+    for font_path in parsed_args.fonts:
+        font = TTFont(font_path)
+        if "GPOS" not in font:
+            print(f"No GPOS in {font_path.name}, skipping.", file=sys.stderr)
+            continue
+        size_orig = len(font.getTableData("GPOS")) / 1024
+        print(f"Measuring {font_path.name}...", file=sys.stderr)
+
+        font_one = TTFont(font_path)
+        compact(font_one, mode="one")
+        font_one_path = font_path.with_name(font_path.stem + "_one" + font_path.suffix)
+        font_one.save(font_one_path)
+        font_one = TTFont(font_one_path)
+        size_one = len(font_one.getTableData("GPOS")) / 1024
+        del font_one
+
+        font_max = TTFont(font_path)
+        compact(font_max, mode="max")
+        font_max_path = font_path.with_name(font_path.stem + "_max" + font_path.suffix)
+        font_max.save(font_max_path)
+        font_max = TTFont(font_max_path)
+        size_max = len(font_max.getTableData("GPOS")) / 1024
+        del font_max
+
+        font_auto = TTFont(font_path)
+        compact(font_auto, mode="auto")
+        font_auto_path = font_path.with_name(
+            font_path.stem + "_auto" + font_path.suffix
+        )
+        font_auto.save(font_auto_path)
+        font_auto = TTFont(font_auto_path)
+        size_auto = len(font_auto.getTableData("GPOS")) / 1024
+        del font_auto
+
+        rows.append(
+            (
+                font_path.name,
+                size_orig,
+                size_one,
+                pct(size_one, size_orig),
+                size_auto,
+                pct(size_auto, size_orig),
+                size_max,
+                pct(size_max, size_orig),
+            )
+        )
+
+    write_csv(rows)
+
+
+def write_csv(rows: List[Tuple[Any]]) -> None:
+    sys.stdout.write("\uFEFF")
+    writer = csv.writer(sys.stdout, lineterminator="\n")
+    writer.writerow(
+        [
+            "File",
+            "Original GPOS Size",
+            "mode=one",
+            "Change one",
+            "mode=auto",
+            "Change auto",
+            "mode=max",
+            "Change max",
+        ]
+    )
+    for row in rows:
+        writer.writerow(row)
+
+
+def pct(new: float, old: float) -> float:
+    return -100 * (1 - (new / old))
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_gpos_compression_by_tetris.py
@@ -0,0 +1,5 @@
+from gpos_compression_by_tetris import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
